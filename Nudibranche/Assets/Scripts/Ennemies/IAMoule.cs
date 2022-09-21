23929f5250784e79eead950e0c9c2a83e2a524bb
VALLEE <m.vallee@rubika-edu.com>
2022-09-21 14:03:55 +0200

    Moule qui se déplace

--- Nudibranche/Assets/Scripts/Ennemies/IAMoule.cs
+++ Nudibranche/Assets/Scripts/Ennemies/IAMoule.cs
@@ -1,18 +1,41 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class IAMoule : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public GameObject player;
+    public float speed = 1;
+    private SpriteRenderer mouleSprite;
+    private float oldXPos;
+    private float newXPos;
+
+    private void Start()
     {
-        
+        mouleSprite = gameObject.GetComponent<SpriteRenderer>();
+        oldXPos = transform.position.x;
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        transform.position = Vector3.MoveTowards(transform.position, player.transform.position, speed);
+        Flip();
+
+    }
+
+    void Flip()
+    {
+        newXPos = transform.position.x;
+        if (oldXPos<newXPos)
+        {
+            mouleSprite.flipX = true;
+        }
+        else
+        {
+            mouleSprite.flipX = false;
+        }
+
+        oldXPos = newXPos;
     }
 }
