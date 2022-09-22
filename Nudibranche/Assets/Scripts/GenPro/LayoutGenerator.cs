565499c41082ed187da57b7f609439a4d40aa8ec
Maxime <m.philippon@rubika-edu.com>
2022-09-22 17:42:19 +0200

    Fin Setup Gen Pro
    Création Dossiers Ressource + Début Script Accès Dossier

--- Nudibranche/Assets/Scripts/GenPro/LayoutGenerator.cs
+++ Nudibranche/Assets/Scripts/GenPro/LayoutGenerator.cs
@@ -13,13 +13,13 @@ public class LayoutGenerator : MonoBehaviour
     private Vector3 _newPos;
     void Start()
     {
-        salle = Resources.LoadAll("test", typeof(GameObject));
+        salle = Resources.LoadAll("StartRoom", typeof(GameObject));
 
         _test = 0;
         var salle1 = salle[_test].GameObject();
         Instantiate(salle1,transform,instantiateInWorldSpace:true);
         
-        _test++;
+        salle = Resources.LoadAll("3Room/SEO", typeof(GameObject));
         var salle2 = salle[_test].GameObject();
         _newPos = new Vector3(0, salle1.transform.lossyScale.y / 2 + salle2.transform.lossyScale.y / 2, 0);
         Instantiate(salle2,_newPos,quaternion.identity,transform);
