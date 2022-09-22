4cfd9bf32ef35abf9840ba0f640eaf12511da5b7
Maxime <m.philippon@rubika-edu.com>
2022-09-22 16:40:08 +0200

    Test Accès Dossier Unity

---
+++ Nudibranche/Assets/Scripts/GenPro/LayoutGenerator.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using Unity.Mathematics;
+using Unity.VisualScripting;
+using UnityEngine;
+using Object = UnityEngine.Object;
+
+public class LayoutGenerator : MonoBehaviour
+{
+    public Object[] salle;
+    private int _test;
+    private Vector3 _newPos;
+    void Start()
+    {
+        salle = Resources.LoadAll("test", typeof(GameObject));
+
+        _test = 0;
+        var salle1 = salle[_test].GameObject();
+        Instantiate(salle1,transform,instantiateInWorldSpace:true);
+        
+        _test++;
+        var salle2 = salle[_test].GameObject();
+        _newPos = new Vector3(0, salle1.transform.lossyScale.y / 2 + salle2.transform.lossyScale.y / 2, 0);
+        Instantiate(salle2,_newPos,quaternion.identity,transform);
+    }
+}
