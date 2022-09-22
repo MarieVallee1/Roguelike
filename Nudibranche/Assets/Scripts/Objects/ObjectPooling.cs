5713289745aa60e3922be02c49cc996396e7b58e
Terri <a.dubuc@rubika-edu.com>
2022-09-22 15:21:20 +0200

    Début PlayerController, New Input Sytem, Objects Pooling

---
+++ Nudibranche/Assets/Scripts/Objects/ObjectPooling.cs
@@ -0,0 +1,51 @@
+
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectPooling : MonoBehaviour
+{
+    public static ObjectPooling instance;
+    private List<GameObject> _pooledObjects = new List<GameObject>();
+    private int _amountToPool = 20;
+
+    [SerializeField] private GameObject bulletPrefab;
+
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        for (int i = 0; i < _amountToPool; i++)
+        {
+            GameObject obj = Instantiate(bulletPrefab);
+            obj.SetActive(false);
+            _pooledObjects.Add(obj);
+        }
+    }
+
+    public GameObject GetPooledObject()
+    {
+        for (int i = 0; i < _pooledObjects.Count; i++)
+        {
+            if (!_pooledObjects[i].activeInHierarchy)
+            {
+                return _pooledObjects[i];
+            }
+        }
+
+        return null;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- Nudibranche/Assets/Scripts/Player/PlayerController.cs
+++ Nudibranche/Assets/Scripts/Player/PlayerController.cs
@@ -1,18 +1,53 @@
-using System.Collections;
-using System.Collections.Generic;
+using System;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
-public class PlayerController : MonoBehaviour
+namespace Player
 {
-    // Start is called before the first frame update
-    void Start()
+    public class PlayerController : MonoBehaviour
     {
         
+        #region Variables
+
+        private Rigidbody2D rb;
+        
+        public PlayerInputActions playerControls;
+        
+        private InputAction _move;
+        private InputAction _fire;
+        
+        private Vector2 _moveDirection = Vector2.zero;
+
+        public float moveSpeed;
+
+        #endregion
+
+        private void Awake()
+        {
+            playerControls = new PlayerInputActions();
+            rb = GetComponent<Rigidbody2D>();
+        }
+
+        private void OnEnable()
+        {
+            _move = playerControls.Player.Move;
+            _move.Enable();
+        }
+
+        private void OnDisable()
+        {
+            _move.Disable();
+        }
+
+        void Update()
+        {
+            _moveDirection = _move.ReadValue<Vector2>();
+        }
+
+        private void FixedUpdate()
+        {
+            rb.velocity = new Vector2(_moveDirection.x * moveSpeed, _moveDirection.y * moveSpeed);
+        }
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
