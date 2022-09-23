953f86756a68135d67f8a1d8090692f17011cc09
Maxime <m.philippon@rubika-edu.com>
2022-09-23 18:46:57 +0200

    Fin du setup des méthodes pour la gen pro

--- Nudibranche/Assets/Scripts/GenPro/LayoutGenerator.cs
+++ Nudibranche/Assets/Scripts/GenPro/LayoutGenerator.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 using Unity.Mathematics;
 using Unity.VisualScripting;
@@ -32,6 +33,15 @@ namespace GenPro
         private Object[] _bigRoom;
         
         public List<GameObject> listSalle;
+        
+        private enum Side
+        {
+            Up,
+            Right,
+            Down,
+            Left,
+            Start
+        }
 
         [SerializeField] private Vector3 startPos;
         private Vector3 _newPos;
@@ -39,24 +49,35 @@ namespace GenPro
         {
             PlaceStartRoom();
             PlaceSecondRoom();
+            TestLayout();
         }
 
-        private void InstantiateRoom(IReadOnlyList<Object> salle,int indexOldRoom,int side)
+        private void TestLayout()
         {
+            Place3RoomNse(1,Side.Left);
+            PlaceBossRoom(2,Side.Up);
+            Place2RoomNo(2,Side.Down);
+            Place2RoomNe(4,Side.Left);
+            Place2RoomSe(5,Side.Up);
+        }
+
+        private void InstantiateRoom(IReadOnlyList<Object> salle,int indexOldRoom,Side side)
+        {
+            Debug.Log(indexOldRoom);
             var chosenRoom = Random.Range(0, salle.Count);
             var newRoom = salle[chosenRoom].GameObject().transform;
             switch (side)
             {
-                case 1:
+                case Side.Up:
                     UpPlacement(listSalle[indexOldRoom].transform,newRoom);
                     break;
-                case 2:
+                case Side.Right:
                     RightPlacement(listSalle[indexOldRoom].transform,newRoom);
                     break;
-                case 3:
+                case Side.Down:
                     DownPlacement(listSalle[indexOldRoom].transform,newRoom);
                     break;
-                case 4:
+                case Side.Left:
                     LeftPlacement(listSalle[indexOldRoom].transform,newRoom);
                     break;
                 default:
@@ -65,23 +86,15 @@ namespace GenPro
             }
             listSalle.Add( Instantiate(salle[chosenRoom].GameObject(),_newPos,quaternion.identity,transform));
         }
-
-        private void PlaceStartRoom()
-        {
-            _startRoom = Resources.LoadAll("StartRoom", typeof(GameObject));
-            InstantiateRoom(_startRoom, 0,0);
-        }
-
-        private void PlaceSecondRoom()
-        {
-            _seoRoom = Resources.LoadAll("3Room/SEO", typeof(GameObject));
-            InstantiateRoom(_seoRoom,0,1);
-        }
-
+        
         private void UpPlacement(Transform oldRoom,Transform newRoom)
         {
             var tempX = oldRoom.position.x;
             var tempY = oldRoom.position.y + oldRoom.lossyScale.y / 2 + newRoom.lossyScale.y / 2;
+            if (oldRoom.gameObject.GetComponent<RefEntryBigRoom>())
+            {
+                tempX = oldRoom.gameObject.GetComponent<RefEntryBigRoom>().entreeSud.transform.position.x;
+            }
             _newPos = new Vector3(tempX,tempY, 0);
         }
         
@@ -89,6 +102,10 @@ namespace GenPro
         {
             var tempX = oldRoom.position.x+ oldRoom.lossyScale.x / 2 + newRoom.lossyScale.x / 2;
             var tempY = oldRoom.position.y;
+            if (oldRoom.gameObject.GetComponent<RefEntryBigRoom>())
+            {
+                tempY = oldRoom.gameObject.GetComponent<RefEntryBigRoom>().entreeOuest.transform.position.y;
+            }
             _newPos = new Vector3(tempX,tempY, 0);
         }
         
@@ -96,6 +113,10 @@ namespace GenPro
         {
             var tempX = oldRoom.position.x;
             var tempY = oldRoom.position.y - oldRoom.lossyScale.x / 2 - newRoom.lossyScale.x / 2;
+            if (oldRoom.gameObject.GetComponent<RefEntryBigRoom>())
+            {
+                tempX = oldRoom.gameObject.GetComponent<RefEntryBigRoom>().entreeNord.transform.position.x;
+            }
             _newPos = new Vector3(tempX,tempY, 0);
         }
         
@@ -103,7 +124,114 @@ namespace GenPro
         {
             var tempX = oldRoom.position.x - oldRoom.lossyScale.x / 2 - newRoom.lossyScale.x / 2;
             var tempY = oldRoom.position.y;
+            if (oldRoom.gameObject.GetComponent<RefEntryBigRoom>())
+            {
+                tempY = oldRoom.gameObject.GetComponent<RefEntryBigRoom>().entreeEst.transform.position.y;
+            }
             _newPos = new Vector3(tempX,tempY, 0);
         }
+        
+        #region PlaceRoom
+        private void PlaceStartRoom()
+        {
+            _startRoom = Resources.LoadAll("StartRoom", typeof(GameObject));
+            InstantiateRoom(_startRoom, 0,Side.Start);
+        }
+        private void PlaceSecondRoom()
+        {
+            _seoRoom = Resources.LoadAll("3Room/SEO", typeof(GameObject));
+            InstantiateRoom(_seoRoom,0,Side.Up);
+        }
+        
+        private void PlaceBossRoom(int indexOldRoom,Side side)
+        {
+            _seoRoom = Resources.LoadAll("BossRoom", typeof(GameObject));
+            InstantiateRoom(_seoRoom,indexOldRoom,side);
+        }
+        
+        private void PlaceCharacterRoom(int indexOldRoom,Side side)
+        {
+            _seoRoom = Resources.LoadAll("BossRoom", typeof(GameObject));
+            InstantiateRoom(_seoRoom,indexOldRoom,side);
+        }
+        
+        private void PlaceSecretRoom(int indexOldRoom,Side side)
+        {
+            _seoRoom = Resources.LoadAll("BossRoom", typeof(GameObject));
+            InstantiateRoom(_seoRoom,indexOldRoom,side);
+        }
+        
+        private void PlaceShopRoom(int indexOldRoom,Side side)
+        {
+            _seoRoom = Resources.LoadAll("BossRoom", typeof(GameObject));
+            InstantiateRoom(_seoRoom,indexOldRoom,side);
+        }
+        
+        private void Place4Room(int indexOldRoom,Side side)
+        {
+            _seoRoom = Resources.LoadAll("4Room", typeof(GameObject));
+            InstantiateRoom(_seoRoom,indexOldRoom,side);
+        }
+        
+        private void Place3RoomNeo(int indexOldRoom,Side side)
+        {
+            _seoRoom = Resources.LoadAll("3Room/NEO", typeof(GameObject));
+            InstantiateRoom(_seoRoom,indexOldRoom,side);
+        }
+        
+        private void Place3RoomNse(int indexOldRoom,Side side)
+        {
+            _seoRoom = Resources.LoadAll("3Room/NSE", typeof(GameObject));
+            InstantiateRoom(_seoRoom,indexOldRoom,side);
+        }
+        
+        private void Place3RoomNso(int indexOldRoom,Side side)
+        {
+            _seoRoom = Resources.LoadAll("3Room/NSO", typeof(GameObject));
+            InstantiateRoom(_seoRoom,indexOldRoom,side);
+        }
+        
+        private void Place3RoomSeo(int indexOldRoom,Side side)
+        {
+            _seoRoom = Resources.LoadAll("3Room/SEO", typeof(GameObject));
+            InstantiateRoom(_seoRoom,indexOldRoom,side);
+        }
+        
+        private void Place2RoomEo(int indexOldRoom,Side side)
+        {
+            _seoRoom = Resources.LoadAll("2Room/EO", typeof(GameObject));
+            InstantiateRoom(_seoRoom,indexOldRoom,side);
+        }
+        
+        private void Place2RoomNe(int indexOldRoom,Side side)
+        {
+            _seoRoom = Resources.LoadAll("2Room/NE", typeof(GameObject));
+            InstantiateRoom(_seoRoom,indexOldRoom,side);
+        }
+        
+        private void Place2RoomNo(int indexOldRoom,Side side)
+        {
+            _seoRoom = Resources.LoadAll("2Room/NO", typeof(GameObject));
+            InstantiateRoom(_seoRoom,indexOldRoom,side);
+        }
+        
+        private void Place2RoomNs(int indexOldRoom,Side side)
+        {
+            _seoRoom = Resources.LoadAll("2Room/NS", typeof(GameObject));
+            InstantiateRoom(_seoRoom,indexOldRoom,side);
+        }
+        
+        private void Place2RoomSe(int indexOldRoom,Side side)
+        {
+            _seoRoom = Resources.LoadAll("2Room/SE", typeof(GameObject));
+            InstantiateRoom(_seoRoom,indexOldRoom,side);
+        }
+        
+        private void Place2RoomSo(int indexOldRoom,Side side)
+        {
+            _seoRoom = Resources.LoadAll("2Room/SO", typeof(GameObject));
+            InstantiateRoom(_seoRoom,indexOldRoom,side);
+        }
+        #endregion
     }
 }
--- Nudibranche/Assets/Scripts/GenPro/RefEntryBigRoom.cs
+++ Nudibranche/Assets/Scripts/GenPro/RefEntryBigRoom.cs
@@ -2,10 +2,13 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class RefEntryBigRoom : MonoBehaviour
+namespace GenPro
 {
-    public GameObject entreeNord;
-    public GameObject entreeSud;
-    public GameObject entreeEst;
-    public GameObject entreeOuest;
+    public class RefEntryBigRoom : MonoBehaviour
+    {
+        public GameObject entreeNord;
+        public GameObject entreeSud;
+        public GameObject entreeEst;
+        public GameObject entreeOuest;
+    } 
 }
