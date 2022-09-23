8114ea7ac7a8f8eab5bcac2fb838d46f2e48b82d
VALLEE <m.vallee@rubika-edu.com>
2022-09-23 10:55:50 +0200

    Se faire courser par une moule

--- Nudibranche/Assets/Scripts/Ennemies/IAMoule.cs
+++ Nudibranche/Assets/Scripts/Ennemies/IAMoule.cs
@@ -6,33 +6,88 @@ using Pathfinding;
 
 public class IAMoule : MonoBehaviour
 {
-    private float oldXPos;
-    private float newXPos;
     private SpriteRenderer mouleSprite;
+
+    public Transform target;
+    public float speed = 200;
+    private float nextWaypointDistance = 1;
+
+    private Path path;
+    private int currentWaypoint = 0;
+    private bool reachedEndOfPath = false;
+
+    private Seeker seeker;
+    private Rigidbody2D rb;
 
     private void Start()
     {
         mouleSprite = gameObject.transform.GetChild(0).GetComponent<SpriteRenderer>();
-        oldXPos = transform.position.x;
+
+        seeker = GetComponent<Seeker>();
+        rb = GetComponent<Rigidbody2D>();
+
+        InvokeRepeating("UpdatePath", 0, .5f);
+        
     }
 
-    void Update()
+    void UpdatePath()
+    {
+        if (seeker.IsDone())
+        {
+            seeker.StartPath(rb.position, target.position, OnPathComplete);
+        }
+           
+    }
+
+    void OnPathComplete(Path p)
+    {
+        if (!p.error)
+        {
+            path = p;
+            currentWaypoint = 0;
+        }
+    }
+    void FixedUpdate()
     {
         Flip();
+
+        if (path == null)
+        {
+            return;
+        }
+
+        if (currentWaypoint >= path.vectorPath.Count)
+        {
+            reachedEndOfPath = true;
+            return;
+        }
+        else
+        {
+            reachedEndOfPath = false;
+        }
+
+        Vector2 direction = ((Vector2)path.vectorPath[currentWaypoint] - rb.position);
+        Vector2 force = direction * speed * Time.deltaTime;
+        
+        rb.AddForce(force);
+
+        float distance = Vector2.Distance(rb.position, path.vectorPath[currentWaypoint]);
+
+        if (distance < nextWaypointDistance)
+        {
+            currentWaypoint++;
+        }
     }
 
     void Flip()
     {
-        newXPos = transform.position.x;
-        if (oldXPos<=newXPos)
+        if (rb.velocity.x >= 0.01f)
         {
             mouleSprite.flipX = true;
         }
-        else
+        else if (rb.velocity.x <= 0.01f)
         {
             mouleSprite.flipX = false;
         }
-
-        oldXPos = newXPos;
     }
 } 
