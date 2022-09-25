0251dca0f89d96fdb4aaef39f3521630ef6f2e50
Terri <a.dubuc@rubika-edu.com>
2022-09-25 18:50:16 +0200

    Avancée twinstick création des scriptable objects player data

--- Nudibranche/Assets/Scripts/Player/PlayerController.cs
+++ Nudibranche/Assets/Scripts/Player/PlayerController.cs
@@ -1,6 +1,4 @@
-using System;
 using Objects;
-using Unity.Mathematics;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
@@ -10,88 +8,92 @@ namespace Player
     public class PlayerController : MonoBehaviour
     {
         #region Variables
-
-        private Camera _cam;
-        
-        private Rigidbody2D _rb;
-        [SerializeField] private Transform bulletPosition;
-
-        private PlayerInputActions _playerControls;
-        private InputAction _shoot;
+      
         private Vector2 _movement;
         private Vector2 _aim;
+        
+        private float _nextFireTime;
+        
+        public bool isAttacking;
+        
+        #endregion
+        
+        #region Declaration
+        
+        [SerializeField] private PlayerData playerData;
+        [SerializeField] private Transform bulletPosition;
+        private Rigidbody2D _rb;
+        private PlayerInputActions _playerControls;
 
-        public float rightStickDeadzone; 
-        public float moveSpeed;
-        public float projectileSpeed;
         #endregion
 
         private void Awake()
         {
             _playerControls = new PlayerInputActions();
-
             _rb = GetComponent<Rigidbody2D>();
         }
-
         private void OnEnable()
         {
             _playerControls.Enable();
-
+            
             _playerControls.Player.Movement.performed += OnMovement;
-            _playerControls.Player.Movement.canceled += OnMovement;
             _playerControls.Player.Aim.performed += OnAim;
-            _playerControls.Player.Shoot.performed += ctx => OnShoot();
-            _playerControls.Player.Shoot.canceled += ctx => OnShoot();
+            _playerControls.Player.Shoot.performed += ctx => isAttacking = true;
+            _playerControls.Player.Shoot.canceled += ctx => isAttacking = false;
         }
-
         private void OnDisable()
         {
             _playerControls.Disable();
         }
+        private void Update()
+        {
+            BasicAttackCooldown();
 
+            if (isAttacking)
+            {
+                BasicAttack();
+            }
+        }
         private void FixedUpdate()
         {
             HandleMovement();
             HandleRotation();
         }
-
-        private void Update()
-        {
-            Debug.Log(_aim);
-        }
-
-        void OnShoot()
+        void BasicAttack()
         {
             GameObject bullet = ObjectPooling.instance.GetPooledObject();
-            
-            if (bullet != null)
+
+            if (bullet != null && BasicAttackCooldown())
             {
                 bullet.transform.position = bulletPosition.position;
                 bullet.SetActive(true);
-                bullet.GetComponent<Rigidbody2D>().velocity = _aim.normalized * projectileSpeed;
+                bullet.GetComponent<Rigidbody2D>().velocity = _aim.normalized * playerData.basicAttackSpeed;
+                _nextFireTime = Time.time + playerData.basicAttackCooldown;
             }
         }
-        
         void OnMovement(InputAction.CallbackContext context)
         {
             _movement = context.ReadValue<Vector2>();
         }
-
         void OnAim(InputAction.CallbackContext context)
         {
             _aim = (context.ReadValue<Vector2>());
         }
-
         void HandleMovement()
         {
-            _rb.velocity = new Vector2(_movement.x * moveSpeed, _movement.y * moveSpeed);
+            _rb.velocity = new Vector2(_movement.x * playerData.characterSpeed, _movement.y * playerData.characterSpeed);
         }
-
         void HandleRotation()
         {
             float a = Mathf.Atan2(_aim.x, _aim.y) * Mathf.Rad2Deg;
             _rb.MoveRotation(-a);
         }
+        bool BasicAttackCooldown()
+        {
+            if(Time.time > _nextFireTime) return true;
+            return false;
+        }
+
     }
 
 }
