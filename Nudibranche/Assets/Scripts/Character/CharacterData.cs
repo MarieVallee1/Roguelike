6d7b9c7da9721f90f5d168e6c8b4cec50794241d
Terri <a.dubuc@rubika-edu.com>
2022-10-03 19:58:04 +0200

    Progress

---
+++ Nudibranche/Assets/Scripts/Character/CharacterData.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+
+namespace Character
+{
+    [CreateAssetMenu(menuName = "Data", fileName = "PlayerData", order = 0)]
+    public class CharacterData : ScriptableObject
+    {
+
+        [Range(0f, 100f)]
+        public float speed;
+        [Range(0f, 100f)]
+        public float health;
+    
+    }
+}
---
+++ Nudibranche/Assets/Scripts/Objects/PoolingSystem.cs
@@ -0,0 +1,66 @@
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PoolingSystem: MonoBehaviour
+{
+    public static PoolingSystem instance;
+
+    private Dictionary<string, List<GameObject>> _poolDictionary = new Dictionary<string, List<GameObject>>();
+    
+    [SerializeField] private List<PoolData> poolData;
+
+    private void Awake()
+    {
+        if (instance == null) instance = this;
+        
+        InitialInstantiation();
+    }
+
+    void InitialInstantiation()
+    {
+        for (int j = 0; j < poolData.Count; j++)
+        {
+            _poolDictionary.Add(poolData[j].Prefab.name, new List<GameObject>());
+            
+            for (int i = 0; i < poolData[j].NumberOfObjects; i++)
+            {
+                GameObject obj = Instantiate(poolData[j].Prefab);
+                obj.SetActive(false);
+                _poolDictionary[poolData[j].Prefab.name].Add(obj);
+            }
+        }
+    }
+
+    public GameObject GetObject(string objectName)
+    {
+        if (_poolDictionary.ContainsKey(objectName))
+        {
+            for (int i = 0; i < _poolDictionary[objectName].Count; i++)
+            {
+                if (!_poolDictionary[objectName][i].activeInHierarchy)
+                {
+                    return _poolDictionary[objectName][i];
+                }
+            }
+        }
+        return null;
+    }
+}
+
+
+//It allows me to have a custom list of elements in the inspector
+[Serializable]
+public class PoolData
+{
+    [TextArea]
+    public string description;
+    [SerializeField]
+    private GameObject prefab;
+    [SerializeField]
+    private int numberOfObjects;
+        
+    public GameObject Prefab => prefab; //Get without Set 
+    public int NumberOfObjects => numberOfObjects; //Get without Set 
+
+}
