51f5888f6b85509f2f64e5a253c3dce9c43a6a04
VALLEE <m.vallee@rubika-edu.com>
2022-10-11 13:31:25 +0200

    Spawn Oursins

---
+++ Nudibranche/Assets/Scripts/Enemies/ShootOursins.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShootOursins : StateMachineBehaviour
+{
+    private Cannonier cannonier;
+    // OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
+    override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        cannonier = animator.GetComponent<Cannonier>();
+        cannonier.Attaque();
+    }
+
+    // OnStateUpdate is called on each Update frame between OnStateEnter and OnStateExit callbacks
+    //override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    //{
+        
+    //}
+
+    // OnStateExit is called when a transition ends and the state machine finishes evaluating this state
+    //override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    //{
+       
+    //}
+}
