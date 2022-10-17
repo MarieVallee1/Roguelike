c5b340097659b99b77d615a4bd36a9e3d60c494d
VALLEE <m.vallee@rubika-edu.com>
2022-10-17 18:06:43 +0200

    Order in layer en fonction de la position

---
+++ Nudibranche/Assets/Scripts/LayerOrder.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LayerOrder : MonoBehaviour
+{
+    private SpriteRenderer spriteRenderer;
+    private int roundedYPos;
+    void Start()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+    void Update()
+    {
+        roundedYPos = (int)Mathf.Round(transform.position.y * 10);
+        spriteRenderer.sortingOrder = - roundedYPos;
+    }
+}
