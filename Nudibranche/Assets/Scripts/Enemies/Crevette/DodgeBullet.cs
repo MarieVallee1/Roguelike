328dfb8248dc2cff13de85c6cdcbf59fe4d7934c
VALLEE <m.vallee@rubika-edu.com>
2022-11-04 11:06:06 +0100

    La crevette détecte les projectiles qui vont lui faire des dégâts

--- Nudibranche/Assets/Scripts/Enemies/Crevette/DodgeBullet.cs
+++ Nudibranche/Assets/Scripts/Enemies/Crevette/DodgeBullet.cs
@@ -1,16 +1,27 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using Projectiles;
 using UnityEngine;
 
 public class DodgeBullet : MonoBehaviour
 {
     private GameObject bullet;
+    private Vector2 direction;
+
+    private bool bulletDetected;
     private void OnTriggerEnter2D(Collider2D col)
     {
         if (col.CompareTag("PlayerProjectile"))
         {
-            Debug.Log("dodge");
+            bulletDetected = true;
+            bullet = col.gameObject;
+            direction = col.GetComponent<BasicAttack>().direction;
+
+            if (Physics2D.Raycast(col.transform.position, direction, 5, LayerMask.GetMask("ProjectileHitEnnemy")))
+            {
+                Debug.Log("Dodge)");
+            }
         }
     }
 }
