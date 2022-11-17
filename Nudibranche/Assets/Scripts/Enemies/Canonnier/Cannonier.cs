3fc28f4b415c8fa8a5bede9f8f0bf5601cc67d87
VALLEE <m.vallee@rubika-edu.com>
2022-11-17 18:06:45 +0100

    Récupération loot

--- Nudibranche/Assets/Scripts/Enemies/Canonnier/Cannonier.cs
+++ Nudibranche/Assets/Scripts/Enemies/Canonnier/Cannonier.cs
@@ -30,9 +30,6 @@ namespace Ennemy
 
     // Animator //
     public Animator animator;
-    
-    // Drop //
-    
     private void Start()
     {
         target = PlayerController.instance.transform;
