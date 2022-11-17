5b50f9c2321b0cc1d29efa4ad0c63b5fa34478d3
VALLEE <m.vallee@rubika-edu.com>
2022-11-17 14:40:46 +0100

    Animator crevette

---
+++ Nudibranche/Assets/EventManagerCrevette.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using Ennemy;
+using UnityEngine;
+
+public class EventManagerCrevette : MonoBehaviour
+{
+    [SerializeField] private Crevette crevette;
+
+    public void Shoot()
+    {
+        crevette.Shoot();
+    }
+
+    public void DodgeEnd()
+    {
+        crevette.isDodging = false;
+    }
+
+}
