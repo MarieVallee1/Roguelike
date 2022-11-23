a079159ab094393a0d158627b644251f03379d89
VALLEE <m.vallee@rubika-edu.com>
2022-11-23 10:27:47 +0100

    Boss terminé

---
+++ Nudibranche/Assets/BossDamages.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using Character;
+using UnityEngine;
+
+public class BossDamages : MonoBehaviour
+{
+    public int damage = 1;
+    private void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.CompareTag("Player"))
+        {
+            PlayerController.instance.TakeDamage(damage);
+        }
+    }
+}
