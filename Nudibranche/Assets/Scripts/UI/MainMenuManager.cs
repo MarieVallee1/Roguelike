ad33bceec0a0e454f9da7c044c78dbd29d5d19e1
Terri <a.dubuc@rubika-edu.com>
2022-11-30 12:05:09 +0100

    Prog UI Main Menu + Modif moule prefab +speed +linear drag

--- Nudibranche/Assets/Scripts/UI/MainMenuManager.cs
+++ Nudibranche/Assets/Scripts/UI/MainMenuManager.cs
@@ -1,90 +1,182 @@
-using System.Collections;
 using DG.Tweening;
 using UnityEngine;
 using UnityEngine.EventSystems;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
-public class MainMenuManager : MonoBehaviour
+namespace UI
 {
-    private PlayerInputActions _inputActions;
-    private EventSystem _event;
-    [SerializeField] private RectTransform cursor;
-    private string _selectedButtons;
-    public Image blackScreen;
-    private void Awake()
+    public class MainMenuManager : MonoBehaviour
     {
-        _inputActions = new PlayerInputActions();
-        _event = EventSystem.current;
-        
-        //set the alpha to 1
-        blackScreen.color = new Color(0, 0, 0, 1);
-    }
+        private PlayerInputActions _inputActions;
+        private EventSystem _event;
 
-    private void OnEnable()
-    {
-        _inputActions.Character.Disable();
-        _inputActions.UI.Enable();
-    }
+        [Header("Menu References")]
+        [SerializeField] private RectTransform cursor;
+        public Image blackScreen;
+        [SerializeField] private GameObject mainMenu;
+        private CanvasGroup _mainMenuCanvas;
+        [SerializeField] private GameObject optionMenu;
+        private CanvasGroup _optionMenuCanvas;
 
-    void Start()
-    {
-        blackScreen.DOFade(0, 2f).onComplete = DisableBlackScreen;
-    }
+        [SerializeField] private GameObject optionButton;
+        [SerializeField] private GameObject firstOptionSelected;
+        [SerializeField] private GameObject optionReturnButton;
 
-    private void Update()
-    {
-        HandleSelectedButtons();
-    }
-
-    private void DisableBlackScreen()
-    {
-        blackScreen.enabled = false;
-    }
-
-    public void StartButton()
-    {
-        blackScreen.DOKill();
-        blackScreen.enabled = true;
-        blackScreen.DOFade(1, 3f).onComplete = LaunchGame;
-    }
-
-    public void OptionsButton()
-    {
-        
-    }
-
-    public void QuitButton()
-    {
-        Application.Quit();
-    }
-
-    private void LaunchGame()
-    {
-        SceneManager.LoadScene("Scene_Terri");
-    }
-
-    private void HandleSelectedButtons()
-    {
-        switch (_event.currentSelectedGameObject.name)
+        private string _selectedButtons;
+    
+        [Header("States")]
+        public bool mainMenuOpen;
+        public bool optionMenuOpen;
+    
+    
+        private void Awake()
         {
-            case "PlayButton":
+            _inputActions = new PlayerInputActions();
+            _event = EventSystem.current;
+        
+            //set the alpha to 1
+            blackScreen.color = new Color(0, 0, 0, 1);
+        
+            _mainMenuCanvas = mainMenu.GetComponent<CanvasGroup>();
+            _optionMenuCanvas = optionMenu.GetComponent<CanvasGroup>();
+        }
+        private void OnEnable()
+        {
+            //Activates the UI Inputs
+            _inputActions.Character.Disable();
+            _inputActions.UI.Enable();
+
+            _inputActions.UI.Interact.performed += ctx =>
             {
-                cursor.DOAnchorPosY(0, 0.5f);
-            }
-                break;
+                if(_event.currentSelectedGameObject == optionButton) OpenOptionsButton();
+                else if(_event.currentSelectedGameObject == optionReturnButton)QuitOptionsButton();
+                
+                //Moves the cursor depending on the button selected
+                switch (_event.currentSelectedGameObject.name)
+                {
+                    case "PlayButton":
+                    {
+                        StartButton();
+                    }
+                        break;
             
-            case "OptionsButton":
-            {
-                cursor.DOAnchorPosY(-80, 0.5f);
-            }
-                break;
+                    case "OptionsButton":
+                    {
+                        OpenOptionsButton();
+                    }
+                        break;
             
-            case "QuitButton":
+                    case "QuitButton":
+                    {
+                        QuitButton();
+                    }
+                        break;
+                    
+                    case "ReturnButton":
+                    {
+                        QuitOptionsButton();
+                    }
+                        break;
+                }
+            };
+        }
+        void Start()
+        {
+            //Manages the first black screen
+            blackScreen.DOFade(0, 2f).onComplete = DisableBlackScreen;
+
+            mainMenuOpen = true;
+            optionMenuOpen = false;
+        }
+        private void Update()
+        {
+            //Manages the cursor in the main menu
+            if(mainMenuOpen) HandleSelectedButtons();
+        }
+
+
+
+        public void StartButton()
+        {
+            //Fades to black before launching the game
+            blackScreen.DOKill();
+            blackScreen.enabled = true;
+            blackScreen.DOFade(1, 3f).onComplete = LaunchGame;
+            _inputActions.Disable();
+        }
+        public void OpenOptionsButton()
+        {
+            //Fades one menu to let the other appear
+            _mainMenuCanvas.DOFade(0, 0.5f).onComplete = DisableMainMenu;
+            optionMenu.SetActive(true);
+            _optionMenuCanvas.DOFade(1, 0.5f);
+            optionMenuOpen = true;
+            
+            _event.SetSelectedGameObject(firstOptionSelected);
+        }
+        public void QuitOptionsButton()
+        {
+            //Fades one menu to let the other appear
+            _optionMenuCanvas.DOFade(0, 0.5f).onComplete = DisableOptionsMenu;
+            mainMenu.SetActive(true);
+            _mainMenuCanvas.DOFade(1, 0.5f);
+            mainMenuOpen = true;
+
+            _event.SetSelectedGameObject(optionButton);
+        }
+        public void QuitButton()
+        {
+            //Quits the game
+            Application.Quit();
+        }
+
+
+        private void LaunchGame()
+        {
+            //Launches the game
+            SceneManager.LoadScene("Scene_Terri");
+        }
+        private void DisableMainMenu()
+        {
+            mainMenu.SetActive(false);
+            mainMenuOpen = false;
+        }
+        private void DisableOptionsMenu()
+        {
+            optionMenu.SetActive(false);
+            optionMenuOpen = false;
+        }
+        private void DisableBlackScreen()
+        {
+            blackScreen.enabled = false;
+        }
+    
+    
+    
+        private void HandleSelectedButtons()
+        {
+            //Moves the cursor depending on the button selected
+            switch (_event.currentSelectedGameObject.name)
             {
-                cursor.DOAnchorPosY(-160, 0.5f);
+                case "PlayButton":
+                {
+                    cursor.DOAnchorPosY(0, 0.5f);
+                }
+                    break;
+            
+                case "OptionsButton":
+                {
+                    cursor.DOAnchorPosY(-80, 0.5f);
+                }
+                    break;
+            
+                case "QuitButton":
+                {
+                    cursor.DOAnchorPosY(-160, 0.5f);
+                }
+                    break;
             }
-                break;
         }
     }
 }
