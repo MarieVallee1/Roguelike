e869db732a9cb00a497c50030b7096c1e02711a7
VALLEE <m.vallee@rubika-edu.com>
2023-01-17 14:13:46 +0100

    Début intro

---
+++ Nudibranche/Assets/Scripts/Narration/IntroductionCinematic.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using Character;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class IntroductionCinematic : MonoBehaviour
+{
+    public Image[] images;
+    [TextArea(3, 10)]
+    public string[] narratorTexts;
+
+    public CanvasGroup imagesGroup;
+
+    /*If(PlayerController.instance.characterinput.character.interact.triggered && PlayerController.instance.characterinput.UI.interact.triggered)
+    {
+        
+    } */
+}
