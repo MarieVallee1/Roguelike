0c3cd31a9a45cab28129c989c40ca85a6e28f4b1
Terri <a.dubuc@rubika-edu.com>
2023-01-20 14:17:00 +0100

    Ajout dash cooldown feedback

---
+++ Nudibranche/Assets/DashCooldownFeedback.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using Character;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DashCooldownFeedback : MonoBehaviour
+{
+    private Image _barre;
+    // Start is called before the first frame update
+    void Start()
+    {
+        _barre = GetComponent<Image>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log(Time.time / PlayerController.Instance.nextTimeDash);
+        Debug.Log(Time.time);
+        Debug.Log( PlayerController.Instance.nextTimeDash);
+        _barre.fillAmount = Time.time / PlayerController.Instance.nextTimeDash;
+    }
+}
