c58632cf0ecde2397a9b07b10e05193b3adebec0
Terri <a.dubuc@rubika-edu.com>
2023-01-22 18:57:58 +0100

    oublie

--- Nudibranche/Assets/Scripts/PostProcessing.cs
+++ Nudibranche/Assets/Scripts/PostProcessing.cs
@@ -40,7 +40,7 @@ public class PostProcessing : MonoBehaviour
 
     private void PlayerShootFeedback()
     {
-        if (PlayerController.Instance.onShoot && _chromaticAberration.intensity.value < 0.5f)
+        if (PlayerController.Instance.onShoot && _chromaticAberration.intensity.value < 0.3f)
         {
             _chromaticAberration.intensity.value += Time.deltaTime;
         }
